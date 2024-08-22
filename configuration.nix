8d6d1806f438656b2c0c47c392c2f3e74011f833
ivanjermakov <ivanjermakov1@gmail.com>
2024-08-22 15:13:37 +0200

    Symlink `configuration.nix`

--- configuration.nix
+++ configuration.nix
@@ -1,17 +1,1 @@
-{ config, pkgs, ... }:
-{
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  users.users.ivan = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    packages = with pkgs; [
-      cowsay
-      lolcat
-    ];
-    initialPassword = "test";
-  };
-
-  system.stateVersion = "23.11";
-}
+/etc/nixos/configuration.nix
